c46a0ba0faad8aa9215fb3d7153007266af8ed41
goandreus <andres.chavez.crisostomo@gmail.com>
2020-12-11 13:42:26 -0500

    change order main (#7)

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:slivers/sliver_appbar_page.dart';
 import 'home_page.dart';
 
 void main() {
@@ -15,7 +16,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: HomePage(),
+      home: SliverAppBarPage(),
     );
   }
 }
